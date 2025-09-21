28dfcaa7d121194885f522d41ee6629d612558cd
Yahya Mohamed Eisa <91888199+Yahya-eisa@users.noreply.github.com>
2025-09-21 20:41:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -114,10 +114,9 @@ def df_to_pdf_table(df, title="FLASH"):
         data.append([Paragraph(fix_arabic("" if pd.isna(row[col]) else str(row[col])), styleN)
                      for col in df.columns])
 
-   col_widths_cm = [2, 2, 0.1, 4, 2.5, 3, 1.5, 1.5, 3, 4, 1.5, 1.5, 1, 1.5]
-   col_widths = [c * 28.35 for c in col_widths_cm]
+    col_widths_cm = [2, 2, 0.1, 4, 2.5, 3, 1.5, 1.5, 3, 4, 1.5, 1.5, 1, 1.5]
+    col_widths = [c * 28.35 for c in col_widths_cm]
 
-    
     # استخدام التوقيت المحلي لمصر
     tz = pytz.timezone('Africa/Cairo')
     today = datetime.datetime.now(tz).strftime("%Y-%m-%d")
@@ -219,5 +218,3 @@ if uploaded_files:
             file_name=file_name,
             mime="application/pdf"
         )
-
-
